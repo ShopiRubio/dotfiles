51b19dfbe83ff97f3e3edb1756da2288d360faa4
Joel Rubio <113637686+ShopiRubio@users.noreply.github.com>
2022-10-19 12:01:15 -0700

    Update inventory.sh

--- personal/inventory.sh
+++ personal/inventory.sh
@@ -1,4 +1,4 @@
 bin/rails dev:products:create SHOP_ID=1 NUM=5
 bin/rails dev:purchase_orders:create SHOP_ID=1 NUM=5 MAX_ITEMS=10
+bin/rails dev:transfers:create SHOP_ID=1 NUM=5 MAX_ITEMS=10
 bin/rails dev:inventory:movements:create SHOP_ID=1 NUM=5
-# bin/rails dev:transfers:create SHOP_ID=1 NUM=5 MAX_ITEMS=10
