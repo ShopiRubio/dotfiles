bc4016bb6ddf55eeb92d1277a73fa84e9e5dfaed
Joel Rubio <113637686+ShopiRubio@users.noreply.github.com>
2022-10-20 15:25:11 -0700

    Update install.sh

--- personal/install.sh
+++ personal/install.sh
@@ -1,4 +1,4 @@
 # Custom actions to take on initial install of dotfiles.
 # This runs after default install actions, so you can overwrite changes it makes if you want.
 export BUILDKITE_TOKEN="$(cat /etc/spin/secrets/buildkite)"
-echo "Access to Buildkite granted. You can now call `bin/rails test <buildkite_id>`"
+echo "Access to Buildkite granted. You can now call bin/rails test buildkite_id"
