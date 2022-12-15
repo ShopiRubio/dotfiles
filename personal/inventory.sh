bce5bbfb9a5cbc03174018880ab5bb8ba40eaaa1
Joel Rubio <113637686+ShopiRubio@users.noreply.github.com>
2022-12-14 20:32:45 -0400

    Update inventory.sh

--- personal/inventory.sh
+++ personal/inventory.sh
@@ -1,7 +1,7 @@
 # bin/rails dev:betas:enable SHOP_ID=1 BETA=double_sync_transfer_and_movements
 # bin/rails dev:betas:enable SHOP_ID=1 BETA=inventory_transfers_on_movements
 bin/rails dev:betas:enable SHOP_ID=1 BETA=purchase_orders
-bin/rails dev:products:create SHOP_ID=1 NUM=5
+bin/rails dev:products:create SHOP_ID=1 NUM=5 MIN_VARIANTS=5 MAX_VARIANTS=10
 bin/rails dev:locations:create SHOP_ID=1 NUM=4
 bin/rails dev:purchase_orders:create SHOP_ID=1 NUM=5 MAX_ITEMS=10
 # bin/rails dev:inventory:movements:create SHOP_ID=1 NUM=5 MAX_ITEMS=10
