5f6d89959c397faaca5c4b572bc7e0d3a19f0623
Joel Rubio <113637686+ShopiRubio@users.noreply.github.com>
2023-01-12 08:16:50 -0400

    Add more aliases

--- personal/custom.zsh
+++ personal/custom.zsh
@@ -4,4 +4,17 @@
 # over any other customization.
 
 alias g='git'
+
+alias dump='bin/rails graphql:schema:dump'                          # Update GraphQL schema. Use `dump admin` for admin only!
+
+alias test='dev test --include-branch-commits'                      # Run rails backend tests for committed changes
+alias style='dev style -a --include-branch-commits'                 # Style and format code on committed changes
+alias typecheck='bin/typecheck'                                     # :sorbet: Typecheck to ensure signatures don't go stale
+alias rbis='bin/tapioca dsl'                                        # To generate the RBIs for Rails and other DSLs
+
+alias checks='style && typecheck && test'                           # Use this before every push!
+
+
 alias inv='~/dotfiles/personal/inventory.sh'
+alias ebeta='bin/rails dev:betas:enable SHOP_ID=1 BETA='            # Enable a beta flag for Shop 1
+alias dbeta='bin/rails dev:betas:disable SHOP_ID=1 BETA='           # Disable a beta flag for Shop 1
