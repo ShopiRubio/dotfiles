2d75cdf323c9e6e84f78c419a143d633a8735d73
Joel Rubio <113637686+ShopiRubio@users.noreply.github.com>
2023-02-14 11:19:03 -0500

    Update custom.zsh

--- personal/custom.zsh
+++ personal/custom.zsh
@@ -8,12 +8,12 @@ alias g='git'
 alias dump='bin/rails graphql:schema:dump'                          # Update GraphQL schema.
 alias migrate='bin/rails db:migrate'                                # Run db migrations.
 alias reindex='rake elasticsearch:reindex'                          # Before reindexing elastic search, comment out the mappings not needed in elastic_search_config.rb
-alias glint='dev graphqllint'                                       # Check for GraphQL Lint violations.
 
 alias test='dev test --include-branch-commits'                      # Run rails backend tests for committed changes
 alias style='dev style -a --include-branch-commits'                 # Style and format code on committed changes
 alias typecheck='bundle exec srb tc'                                # :sorbet: Typecheck to ensure signatures don't go stale
 alias rbis='bin/tapioca dsl'                                        # To generate the RBIs for Rails and other DSLs
+alias glint='dev graphqllint'                                       # Check for GraphQL Lint violations.
 
 alias checks='style && typecheck && test'                           # Use this before every push!
 
