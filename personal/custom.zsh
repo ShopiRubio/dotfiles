e6340f64c6c28384aa5a6ececd36ec82af85567a
Joel Rubio <113637686+ShopiRubio@users.noreply.github.com>
2023-05-30 10:55:29 -0700

    Update custom.zsh

--- personal/custom.zsh
+++ personal/custom.zsh
@@ -8,7 +8,8 @@ alias g='git'
 alias dump='bin/rails graphql:schema:dump'                          # Update GraphQL schema.
 alias dumpa='dev dump-graphql admin'                                # Update Admin GraphQL schema only.
 alias migrate='bin/rails db:migrate'                                # Run db migrations.
-alias reindex='rake elasticsearch:reindex'                          # Before reindexing elastic search, comment out the mappings not needed in elastic_search_config.rb
+alias test_es='bundle exec rake elasticsearch:update_test_mappings' # Generates elastic search test mappings.
+alias reindex='rake elasticsearch:reindex'                          
 
 alias test='dev test --include-branch-commits'                      # Run rails backend tests for committed changes
 alias coverage='dev test --include-branch-commits --coverage'       # Run rails backend tests for committed changes AND check coverage
