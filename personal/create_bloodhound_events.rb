5ae72b0dfa4e1d9aa5c2029543318073748964ab
Joel Rubio <113637686+ShopiRubio@users.noreply.github.com>
2023-06-13 15:20:51 -0700

    Create create_bloodhound_events.rb

---
+++ personal/create_bloodhound_events.rb
@@ -0,0 +1,23 @@
+include FactoryBot::Syntax::Methods
+include ActionDispatch::TestProcess
+
+tracker = Tracker.last
+
+def create_event(*args)
+  event_record = build(*args)
+  tracker = event_record.tracker
+  unless tracker.persisted?
+    tracker.save!
+    event_record.tracker_id = tracker.id
+  end
+  now = Time.current
+  event_record.created_at = now
+  event_record.updated_at = now
+  Db:: Events::Facade.insert(tracker_id: tracker.id, events: [event_record.attributes])
+  events Db::Events::Facade.fetch(tracker.id)
+  events.sort_by(&:created_at).last
+end
+
+event_1 = create_event(:tracking_event, tracker_id: tracker.id, status: Enum::TrackingStatuses::OUT_FOR_DELIVERY, happened at: Time.current)
+event_2 = create_event(:tracking_event, tracker_id: tracker.id, status: Enum::TrackingStatuses::DELAYED, happened at: Time.current)
+event_3 = create_event(:tracking_event, tracker_id: tracker.id, status: Enum::TrackingStatuses::DELIVERED, happened at: Time.current)
