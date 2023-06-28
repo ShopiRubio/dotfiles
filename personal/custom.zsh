5b7ee2023168bc6b2de9f52a3a0caf82a98ca405
Joel Rubio <113637686+ShopiRubio@users.noreply.github.com>
2023-06-28 16:52:15 -0700

    Update gfu so it uses current branch

--- personal/custom.zsh
+++ personal/custom.zsh
@@ -36,8 +36,8 @@ alias ebeta='f(){ bin/rails dev:betas:enable SHOP_ID=1 BETA="$@";  unset -f f; }
 # Disable a beta flag for Shop 1
 alias dbeta='f(){ bin/rails dev:betas:disable SHOP_ID=1 BETA="$@";  unset -f f; }; f'         
 
-# git force update a branch
-alias gfu='f(){ g fo main && g rebase origin/main && g push origin +"$@";  unset -f f; }; f'
+# git force update currently checked out branch
+alias gfu='f(){ g fo main && g rebase origin/main && g push origin -f $(git rev-parse --abbrev-ref HEAD);  unset -f f; }; f'
 
 
 ### Monorail
