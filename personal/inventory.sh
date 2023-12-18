7419f3d3af07690298e4267243ccf63c99e34660
Joel Rubio <113637686+ShopiRubio@users.noreply.github.com>
2023-12-18 08:55:57 -0800

    Delete personal/inventory.sh

--- personal/inventory.sh
+++
@@ -1,5 +0,0 @@
-# bin/rails dev:betas:enable SHOP_ID=1 BETA=
-bin/rails dev:products:create SHOP_ID=1 NUM=5 MIN_VARIANTS=5 MAX_VARIANTS=10
-bin/rails dev:locations:create SHOP_ID=1 NUM=4
-bin/rails dev:purchase_orders:create SHOP_ID=1 NUM=5 MAX_ITEMS=10
-# bin/rails dev:inventory:movements:create SHOP_ID=1 NUM=5 MAX_ITEMS=10
